1f02d422ab5c716947b821d964480eb56fadd559
OTAIZA ORELLANO ALEJANDRO <aotaiza@cuc.edu.co>
2022-03-02 19:55:34 -0500

    Se agrego el punto 2 y 3 de los ejercicios aun en desarrollo

---
+++ punto3.py
@@ -0,0 +1,8 @@
+sueldo = int(input("Ingrese la cantidad donada: "))
+
+sueldoComicion = sueldo *0.15
+totalSueldo = sueldo+sueldoComicion
+
+print("total del sueldo: ",sueldo)
+print("total de comicion: ",sueldoComicion)
+print("Total del suelto mas la comicion: ",totalSueldo)
