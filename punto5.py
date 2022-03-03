4c55d158e88915906606adb84c03990a355b7239
OTAIZA ORELLANO ALEJANDRO <aotaiza@cuc.edu.co>
2022-03-02 20:44:59 -0500

    Finalizado punto 5, se finalizo el taller

---
+++ punto5.py
@@ -0,0 +1,18 @@
+'''Dada la cantidad de alumnos de Redes, contabilidad y diseño determine el
+porcentaje de alumnos de cada uno de los cursos.'''
+
+redes = int(input("cantidad estudiantes redes: "))
+diseño = int(input("cantidad estudiantes diseño: "))
+contabilidad = int(input("cantidad estudiantes contabilidad: "))
+
+totalEstudiantes= redes+diseño+contabilidad
+
+totalporcentaje1 = (redes/totalEstudiantes)*100
+totalporcentaje2 = (diseño/totalEstudiantes)*100
+totalporcentaje3 = (contabilidad/totalEstudiantes)*100
+
+print("La inversion total de estudiantes es: ",totalEstudiantes)
+
+print("porcentaje estudiantes de redes: ",totalporcentaje1)
+print("porcentaje estudiantes diseño: ",totalporcentaje2)
+print("porcentaje estudiantes contabilidad ",totalporcentaje3)
