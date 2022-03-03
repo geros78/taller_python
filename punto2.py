3eb5c8a1bf3068a5b6f6942944b23fe333b7dd0d
OTAIZA ORELLANO ALEJANDRO <aotaiza@cuc.edu.co>
2022-03-02 20:02:36 -0500

    punto 3 finalizado, se agrego punto 4 y punto 2 en desarrollo

--- punto2.py
+++ punto2.py
@@ -3,12 +3,18 @@ tres personas para formar una empresa. Cada una de ellas invierte una
 cantidad distinta. Imprimir el porcentaje que cada quien invierte con respecto
 al total de la inversión.'''
 
-inversion1 = int(print("Digite la primera inversion: "))
-inversion2 = int(print("Digite la segunda inversion: "))
-inversion3 = int(print("Digite la tercera inversion: "))
+inversion1 = float(print("Digite la primera inversion: "))
+inversion2 = float(print("Digite la segunda inversion: "))
+inversion3 = float(print("Digite la tercera inversion: "))
 
 totalInversion= inversion1+inversion2+inversion3
 
-totalInversion1 = totalInversion /inversion1
-totalInversion2 = totalInversion /inversion2
-totalInversion3 = totalInversion /inversion3
+totalporcentaje1 = totalInversion /inversion1
+totalporcentaje2 = totalInversion /inversion2
+totalporcentaje3 = totalInversion /inversion3
+
+print("La inversion total es: ",totalporcentaje1)
+
+print("porcentaje inversion 1: ",totalporcentaje1)
+print("porcentaje inversion 2: ",totalporcentaje2)
+print("porcentaje inversion 3: ",totalporcentaje3)
--- punto3.py
+++ punto3.py
@@ -1,3 +1,9 @@
+'''Un vendedor recibe un sueldo base más un 15% extra por comisión de sus
+ventas efectuadas en el mes. El vendedor desea saber cuanto dinero en
+total obtendrá por las ventas que realiza en el mes. Desarrolle un programa
+en Python que permita mostrar el valor ganado por comisión y el valor total
+a pagar al vendedor.'''
+
 sueldo = int(input("Ingrese la cantidad donada: "))
 
 sueldoComicion = sueldo *0.15
